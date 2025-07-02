23fb649ccc0e51556b2e63db77343a57455d157c
toltsis <toltsisomri@gmail.com>
2025-07-02 04:17:16 +0300

    bla bla

--- ML/initConfig.py
+++ ML/initConfig.py
@@ -4,10 +4,11 @@ import sys
 
 
 if not os.path.exists("config.cfg"):
-    subprocess.run([sys.executable,
-        "python", "-m", "spacy", "init", "config", "config.cfg",
+    subprocess.run([
+        sys.executable, "-m", "spacy", "init", "config", "config.cfg",
         "--lang", "en", "--pipeline", "ner", "--force"
     ])
+
     print("✅ config.cfg created.")
 else:
     print("⚠️ config.cfg already exists.")
